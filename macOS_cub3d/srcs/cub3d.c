b67a59608deba8a93e7b7cbdc5bd78d06d274119
Julien CHENE <jchene@e2r10p12.42.fr>
2021-02-08 16:06:09 +0100

    added textures

--- macOS_cub3d/srcs/cub3d.c
+++ macOS_cub3d/srcs/cub3d.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 14:35:05 by jchene            #+#    #+#             */
+/*   Updated: 2021/02/08 16:05:47 by jchene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/cub3d.h"
 
 int		check_config(int map_id, t_config *file_info)
@@ -48,23 +60,23 @@ int		init_game_var(t_mlx *mlx_ptrs)
 {
 	char player;
 
-	if (!(mlx_ptrs->game_var = (t_game_var*)malloc(sizeof(t_game_var))))
+	if (!(GAME = (t_game_var*)malloc(sizeof(t_game_var))))
 		return (-1);
-	mlx_ptrs->game_var->block_size = 64;
-	mlx_ptrs->game_var->fov = 60;
-	mlx_ptrs->game_var->move_speed = 10;
-	mlx_ptrs->game_var->angle_speed = 5;
-	mlx_ptrs->game_var->player_cords[0] = (mlx_ptrs->map->player_x * 64 + 32);
-	mlx_ptrs->game_var->player_cords[1] = (mlx_ptrs->map->player_y * 64 + 32);
-	player = mlx_ptrs->map->map[mlx_ptrs->map->player_y][mlx_ptrs->map->player_y];
+	GAME->block_size = 64;
+	GAME->fov = 60;
+	GAME->move_speed = 10;
+	GAME->angle_speed = 5;
+	GAME->player_cords[0] = (MAP->player_x * 64 + 32);
+	GAME->player_cords[1] = (MAP->player_y * 64 + 32);
+	player = MAP->map[MAP->player_y][MAP->player_y];
 	if (player == 'N')
-		mlx_ptrs->game_var->angle = 90.0;
+		GAME->angle = 90.0;
 	else if (player == 'S')
-		mlx_ptrs->game_var->angle = 270.0;
+		GAME->angle = 270.0;
 	else if (player == 'E')
-		mlx_ptrs->game_var->angle = 180.0;
+		GAME->angle = 180.0;
 	else
-		mlx_ptrs->game_var->angle = 0;
+		GAME->angle = 0;
 	return (0);
 }
 
@@ -77,22 +89,22 @@ int		handle_keys(int key, t_mlx *mlx_ptrs)
 		exit(0);
 	}
 	else if (key == 1)
-		mlx_ptrs->game_var->player_cords[1] += mlx_ptrs->game_var->move_speed;
+		GAME->player_cords[1] += GAME->move_speed;
 	else if (key == 13)
-		mlx_ptrs->game_var->player_cords[1] -= mlx_ptrs->game_var->move_speed;
+		GAME->player_cords[1] -= GAME->move_speed;
 	else if (key == 2)
-		mlx_ptrs->game_var->player_cords[0] -= mlx_ptrs->game_var->move_speed;
+		GAME->player_cords[0] -= GAME->move_speed;
 	else if (key == 0)
-		mlx_ptrs->game_var->player_cords[0] += mlx_ptrs->game_var->move_speed;
+		GAME->player_cords[0] += GAME->move_speed;
 	else if (key == 124)
-		mlx_ptrs->game_var->angle = fmod((mlx_ptrs->game_var->angle +
-			mlx_ptrs->game_var->angle_speed), 360.0);
+		GAME->angle = fmod((GAME->angle +
+			GAME->angle_speed), 360.0);
 	else if (key == 123)
 	{
-		mlx_ptrs->game_var->angle = fmod((mlx_ptrs->game_var->angle -
-			mlx_ptrs->game_var->angle_speed), 360.0);
-		if (mlx_ptrs->game_var->angle < 0)
-			mlx_ptrs->game_var->angle += 360;
+		GAME->angle = fmod((GAME->angle -
+			GAME->angle_speed), 360.0);
+		if (GAME->angle < 0)
+			GAME->angle += 360;
 	}
 	return (0);
 }
@@ -112,52 +124,44 @@ int		init_img_data(t_img_data *img_data, t_mlx *mlx_ptrs)
 
 int		draw_column(t_mlx *mlx_ptrs, t_img_data *img_data, int column)
 {
-	//int		length;
 	int		heigth;
 	int		line_count;
+	//int		length;
 
-	//length = mlx_ptrs->config->resolution[0];
-	heigth = mlx_ptrs->config->resolution[1];
+	//length = CONFIG->resolution[0];
+	heigth = CONFIG->resolution[1];
 	/*printf("length: %d - heigth: %d - start: %p\n", length, heigth,
 		img_data->start);
-	printf("ceiling color: %d - %d - %d\n", mlx_ptrs->config->ceiling_color[0],
-		mlx_ptrs->config->ceiling_color[1], mlx_ptrs->config->ceiling_color[2]);*/
+	printf("ceiling color: %d - %d - %d\n", CONFIG->ceiling_color[0],
+		CONFIG->ceiling_color[1], CONFIG->ceiling_color[2]);*/
 	line_count = 0;
 	while (line_count < heigth)
 	{
-		if (line_count < ((heigth / 2) - (mlx_ptrs->calc_var->perc_hei / 2)))
+		if (line_count < ((heigth / 2) - (CALC->perc_hei / 2)))
 		{
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 0] = mlx_ptrs->config->ceiling_color[2];
+				+ 0] = CONFIG->ceiling_color[2];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 1] = mlx_ptrs->config->ceiling_color[1];
+				+ 1] = CONFIG->ceiling_color[1];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 2] = mlx_ptrs->config->ceiling_color[0];
+				+ 2] = CONFIG->ceiling_color[0];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
 				+ 3] = 0;
 		}
-		else if (line_count > ((heigth / 2) + (mlx_ptrs->calc_var->perc_hei / 2)))
+		else if (line_count > ((heigth / 2) + (CALC->perc_hei / 2)))
 		{
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 0] = mlx_ptrs->config->floor_color[2];
+				+ 0] = CONFIG->floor_color[2];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 1] = mlx_ptrs->config->floor_color[1];
+				+ 1] = CONFIG->floor_color[1];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 2] = mlx_ptrs->config->floor_color[0];
+				+ 2] = CONFIG->floor_color[0];
 			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
 				+ 3] = 0;
 		}
 		else
 		{
 			
-			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 0] = 0;
-			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 1] = 32;
-			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 2] = (char)170;
-			img_data->start[((column * 4) + (line_count * *(img_data->ln_size)))
-				+ 3] = 0;
 		}
 		line_count+= 1;
 	}
@@ -165,137 +169,6 @@ int		draw_column(t_mlx *mlx_ptrs, t_img_data *img_data, int column)
 }
 
 float	raycast(t_mlx *mlx_ptrs, int column)
-{
-	float	mapx;
-	float	mapy;
-	float	factor[2];
-	float	mur[4];
-	float	dist[2];
-	float	locangle;
-	//int		wall;
-
-	locangle = (GAME->angle - (GAME->fov / 2)) +
-		(column * (GAME->fov / CONFIG->resolution[0]));
-	printf("tracing from %f %f column: %d at angle: %f\n", GAME->player_cords[0],
-		GAME->player_cords[1], column, locangle);
-	//wall = 0;
-	if (locangle < 180)
-    	mapy = (int)(GAME->player_cords[1] / 64) * 64 - 1;
-	else
-		mapy = (int)(GAME->player_cords[1] / 64) * 64 + 64;
-	if (locangle > 270 || locangle < 90)
-		mapx = GAME->player_cords[0] + fabsf(GAME->player_cords[1] - mapy) /
-			tan((locangle * M_PI) / 180);
-	else
-		mapx = GAME->player_cords[0] - fabsf(GAME->player_cords[1] - mapy) /
-			tan((locangle * M_PI) / 180);
-	factor[0] = (64 / tan((locangle * M_PI) / 180));
-	factor[0] = ((locangle < 90 || locangle > 270) ? (factor[0] * -1) : factor[0]);
-	factor[1] = (locangle < 180 ? 64 : -64);
-	printf("loop with mapx: %f mapy: %f factor: %f %f\n", mapx, mapy, factor[0], factor[1]);
-	printf("checking horizontally map[%d][%d]\n", (int)(mapy/64), (int)(mapx/64));
-    while (mapx > 0 && mapx < ((MAP->map_len - 1) * 64 - 1) &&
-		mapy > 0 && mapy < ((MAP->map_hei - 1) * 64 - 1) &&
-		MAP->map[(int)(mapy/64)][(int)(mapx/64)] != '1')
-    {
-		printf("map: %d %d\nstart: %f %f %d %d\n", (MAP->map_hei - 1) * 64,
-			(MAP->map_len - 1) * 64, mapx, mapy, (int)(mapx/64), (int)(mapy/64));
-		mapx += factor[0];
-		mapy += factor[1];
-		//wall = 1;
-		printf("end: %f %f %d %d\n", mapx, mapy, (int)(mapx/64), (int)(mapy/64));
-    }
-	/*if (wall == 1)
-	{
-		mur[0] = mapx;
-		mur[1] = mapy;
-		dist[0] = sqrt(pow((GAME->player_cords[0] - mur[0]), 2) +
-			pow((GAME->player_cords[1] - mur[1]), 2));
-	}*/
-	if (mapx < 0 || mapy < 0)
-	{
-		if (mapx < 0)
-			mapx = 0;
-		if (mapy < 0)
-			mapy = 0;
-	}
-	if (mapx < ((MAP->map_len - 1) * 64 - 1) &&
-		mapy < ((MAP->map_hei - 1) * 64 - 1) &&
-		MAP->map[(int)(mapy/64)][(int)(mapx/64)] == '1')
-	{
-		mur[0] = mapx;
-		mur[1] = mapy;
-		dist[0] = sqrt(pow((GAME->player_cords[0] - mur[0]), 2) +
-			pow((GAME->player_cords[1] - mur[1]), 2));
-	}
-	else
-		dist[0] = MAP->map_len * 64;
-	printf("dist[0]: %f\n", dist[0]);
-	//wall = 0;
-	if (locangle > 270 || locangle < 90)
-		mapx = (int)(GAME->player_cords[0] / 64) * 64 + 64;
-	else
-		mapx = (int)(GAME->player_cords[0] / 64) * 64 - 1;
-	if (locangle < 180)
-		mapy = GAME->player_cords[1] + fabsf(GAME->player_cords[0] - mapx) *
-			tan((locangle * M_PI) / 180);
-	else
-		mapy = GAME->player_cords[1] - fabsf(GAME->player_cords[0] - mapx) *
-			tan((locangle * M_PI) / 180);
-    factor[0] = ((locangle < 90 || locangle > 270) ? 64 : -64);
-	factor[1] = 64 * tan((locangle * M_PI) / 180);
-	factor[1] = (locangle > 180 ? factor[1] : (-1 * factor[1]));
-	printf("loop with mapx: %f mapy: %f factor: %f %f\n", mapx, mapy, factor[0], factor[1]);
-	printf("checking vertically map[%d][%d]\n", (int)(mapy/64), (int)(mapx/64));
-	while (mapx > 0 && mapx < ((MAP->map_len - 1) * 64 - 1) &&
-		mapy > 0 && mapy < ((MAP->map_hei - 1) * 64 - 1) &&
-		MAP->map[(int)(mapy/64)][(int)(mapx/64)] != '1') 
-	{
-		printf("map: %d %d\nstart: %f %f %d %d\n", (MAP->map_hei - 1) * 64,
-			(MAP->map_len - 1) * 64, mapx, mapy, (int)(mapx/64), (int)(mapy/64));
-		mapx += factor[0];
-		mapy += factor[1];
-		//wall = 1;
-		printf("end: %f %f %d %d\n", mapx, mapy, (int)(mapx/64), (int)(mapy/64));
-	}
-	/*if (wall == 1)
-	{
-		mur[2] = mapx;
-		mur[3] = mapy;
-		dist[1] = sqrt(pow((GAME->player_cords[0] - mur[2]), 2) +
-			pow((GAME->player_cords[1] - mur[3]), 2));
-	}*/
-	if (mapx < 0 || mapy < 0)
-	{
-		if (mapx < 0)
-			mapx = 0;
-		if (mapy < 0)
-			mapy = 0;
-	}
-	if (mapx < ((MAP->map_len - 1) * 64 - 1) &&
-		mapy < ((MAP->map_hei - 1) * 64 - 1) &&
-		MAP->map[(int)(mapy/64)][(int)(mapx/64)] == '1')
-	{
-		mur[2] = mapx;
-		mur[3] = mapy;
-		dist[1] = sqrt(pow((GAME->player_cords[0] - mur[2]), 2) +
-			pow((GAME->player_cords[1] - mur[3]), 2));
-	}
-	else
-		dist[1] = MAP->map_hei * 64;
-	printf("dist[1]: %f\n", dist[1]);
-	printf("short: dist[%d]\n", (dist[0] < dist[1]) ? 0 : 1);
-	printf("dist: %f\n\n", (dist[0] < dist[1]) ? dist[0] : dist[1]);
-	/*if (dist[1] == sqrt(pow((GAME->player_cords[0] - mur[2]), 2)
-		+ pow((GAME->player_cords[1] - mur[3]), 2)))
-		dist[1] = MAP->map_len * 64;
-	if (dist[0] == sqrt(pow((GAME->player_cords[0] - mur[0]), 2)
-		+ pow((GAME->player_cords[1] - mur[1]), 2)))
-		dist[0] = MAP->map_len * 64;*/
-	return ((dist[0] < dist[1]) ? dist[0] : dist[1]);
-}
-
-/*float	raytrace(t_mlx *mlx_ptrs, int column)
 {
 	int		quart;
 	float	local_cords[2];
@@ -313,29 +186,29 @@ float	raycast(t_mlx *mlx_ptrs, int column)
 	while (MAP->map[((int)map_cords[1] / 64)][((int)map_cords[0] / 64)] != '1')
 	{
 		if (locangle < 45.0)
-        {
-            local_cords[0]++;
-            local_cords[1] += (locangle == 0 ? 0 : tan((locangle*M_PI)/180));
-        }
-        else
-        {
-            local_cords[1]++;
-            local_cords[0] += 1/(tan((locangle*M_PI)/180));
-        }
-        if (quart == 0 || quart == 2)
-        {
-            map_cords[0] = GAME->player_cords[0] + (quart == 0 ?
+		{
+			local_cords[0]++;
+			local_cords[1] += (locangle == 0 ? 0 : tan((locangle*M_PI)/180));
+		}
+		else
+		{
+			local_cords[1]++;
+			local_cords[0] += 1/(tan((locangle*M_PI)/180));
+		}
+		if (quart == 0 || quart == 2)
+		{
+			map_cords[0] = GAME->player_cords[0] + (quart == 0 ?
 				local_cords[0] : (local_cords[0] * -1));
-            map_cords[1] = GAME->player_cords[1] - (quart == 0 ?
+			map_cords[1] = GAME->player_cords[1] - (quart == 0 ?
 				local_cords[1] : (local_cords[1] * -1));
-        }
-        else if (quart == 1 || quart == 3)
-        {
-            map_cords[0] = GAME->player_cords[0] + (quart == 1 ?
+		}
+		else if (quart == 1 || quart == 3)
+		{
+			map_cords[0] = GAME->player_cords[0] + (quart == 1 ?
 				(local_cords[1] * -1) : local_cords[1]);
-            map_cords[1] = GAME->player_cords[1] - (quart == 1 ?
+			map_cords[1] = GAME->player_cords[1] - (quart == 1 ?
 				local_cords[0] : (local_cords[0] * -1));
-        }	
+		}	
 	}
 	printf("hit wall from %f %f to %f %f map: %c column: %d\n",
 		GAME->player_cords[0], GAME->player_cords[1], map_cords[0], map_cords[1],
@@ -345,7 +218,7 @@ float	raycast(t_mlx *mlx_ptrs, int column)
 		((map_cords[1] - GAME->player_cords[1])) *
 		(map_cords[1] - GAME->player_cords[1])));
 	return (dist);
-}*/
+}
 
 int		draw_img(t_img_data *img_data, t_mlx *mlx_ptrs)
 {
@@ -354,21 +227,21 @@ int		draw_img(t_img_data *img_data, t_mlx *mlx_ptrs)
 	
 	column = 0;
 	length = *(img_data->ln_size) / (*(img_data->bpp) / 8);
-	if (!(mlx_ptrs->calc_var = (t_calc_var *)malloc(sizeof(t_calc_var))))
+	if (!(CALC = (t_calc_var *)malloc(sizeof(t_calc_var))))
 		return (-1);
-	mlx_ptrs->calc_var->screen_dist = ((length / 2) /
-		(tan((((mlx_ptrs->game_var->fov / 2) * M_PI) / 180))));
+	CALC->screen_dist = ((length / 2) /
+		(tan((((GAME->fov / 2) * M_PI) / 180))));
 	while (column < length)
 	{
-		mlx_ptrs->calc_var->wall_dist = raycast(mlx_ptrs, column);
-		mlx_ptrs->calc_var->perc_hei = (mlx_ptrs->calc_var->screen_dist *
-			(mlx_ptrs->game_var->block_size / mlx_ptrs->calc_var->wall_dist));
+		CALC->wall_dist = raycast(mlx_ptrs, column);
+		CALC->perc_hei = (CALC->screen_dist *
+			(GAME->block_size / CALC->wall_dist));
 		printf("b_size: %d - angle: %f - x: %f - y: %f\n",
-			mlx_ptrs->game_var->block_size, mlx_ptrs->game_var->angle, mlx_ptrs->game_var->player_cords[0], mlx_ptrs->game_var->player_cords[1]);
-		/*printf("----%d %d\n", mlx_ptrs->map->player_x, mlx_ptrs->map->player_y);*/
+			GAME->block_size, GAME->angle, GAME->player_cords[0], GAME->player_cords[1]);
+		/*printf("----%d %d\n", MAP->player_x, MAP->player_y);*/
 		draw_column(mlx_ptrs, img_data, column);
 		//printf("column: %d - hp: %f - start: %p\n", column,
-			//mlx_ptrs->calc_var->perc_hei, img_data->start);
+			//CALC->perc_hei, img_data->start);
 		column++;
 	}
 	return (0);
@@ -435,8 +308,8 @@ int		main(int argc, char **argv)
 		return (free_file(file_info, map_id) + free_map(map_file));
 	if (!(mlx_ptrs = (t_mlx *)ft_calloc(1, sizeof(t_mlx))))
 		return (free_file(file_info, map_id) + free_map(map_file));
-	mlx_ptrs->map = map_file;
-	mlx_ptrs->config = file_info;
+	MAP = map_file;
+	CONFIG = file_info;
 	if ((start_mlx(file_info, mlx_ptrs)) == -1)
 		return (free_file(file_info, map_id) + free_map(map_file));
 	return (0);
